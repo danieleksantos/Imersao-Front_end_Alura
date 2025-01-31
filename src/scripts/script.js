08424e8082e0160a48b0b131e26dc7030d46f595
Daniele Karina dos Santos <danimistron@gmail.com>
2025-01-31 15:29:02 -0300

    update js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -5,14 +5,28 @@ const resultPlaylist = document.getElementById('result-playlists');
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`
     fetch(url)
-        .then()
+        .then((response) => response.json())
+        .then((results) => displayResults(results));
 }
 
-document.addEventListener('input', function(){
+function displayResults(results) {
+    resultPlaylist.classList.add('hidden');
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    results.forEach((element) => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+    resultArtist.classList.remove('hidden');
+}
+
+searchInput.addEventListener("input", function(){
     const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === '') {
-        resultPlaylist.classList.add('hidden');
-        resultArtist.classList.remove('hidden');
-        return
+    if (searchTerm === "") {
+        resultArtist.classList.add("hidden");
+        resultPlaylist.classList.remove("hidden");
+        return;
     }
-})
+    requestApi(searchTerm);
+});
