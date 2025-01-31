b9cd2e503755d24407c716b7f750d1e1b1273b55
Daniele Karina dos Santos <danimistron@gmail.com>
2025-01-31 06:11:08 -0300

    update js

---
+++ src/scripts/script.js
@@ -0,0 +1,18 @@
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById('result-artist');
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then()
+}
+
+document.addEventListener('input', function(){
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.add('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+})
